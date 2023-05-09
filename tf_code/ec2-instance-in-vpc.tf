f1263854e3de8b0ad41befb06725624be68a9ae2
Amar Chand <chanddargad@gmail.com>
2023-05-09 14:55:15 +0530

    tf_code: modified aws_instance
    explicitly set associate_public_ip_address argument to false for aws_instance foo

--- tf_code/ec2-instance-in-vpc.tf
+++ tf_code/ec2-instance-in-vpc.tf
@@ -29,6 +29,8 @@ resource "aws_instance" "foo" {
   ami           = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t2.micro"
 
+  associate_public_ip_address = false
+
   network_interface {
     network_interface_id = aws_network_interface.foo.id
     device_index         = 0
